f01322db509dc424f30a7d69a14f12c394149007
Christopher Holmes <christopher.holmes@newcastle.co.uk>
2021-07-15 20:44:44 +0100

    Added pylint

--- lambda_function.py
+++ lambda_function.py
@@ -1,18 +1,22 @@
+""" Lambda to consume the LTV Calculator """
+
 import os
 from LTVCalculator import LTVCalculator
 
 MAX_LTV = os.environ.get('MAX_LTV')
 
 def lambda_handler(event, context):
+    """ Instantiate calculator and return values in a response object """
+
     loan_amount = event['loan_amount']
     property_value = event['property_value']
     max_ltv = MAX_LTV
-    
+
     ltv_calculator = LTVCalculator(loan_amount, property_value, max_ltv)
-    
+
     ltv_percentage = ltv_calculator.get_value()
     is_acceptable = ltv_calculator.is_acceptable()
-    
+
     data = {
         "ltv_percentage": ltv_percentage,
         "is_acceptable": is_acceptable
@@ -22,4 +26,3 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": data
     }
-    
