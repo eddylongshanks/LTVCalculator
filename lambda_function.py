ec5b22e3b9efeafe807e6484eae9d6ceac3e0c7a
Christopher Holmes <christopher.holmes@newcastle.co.uk>
2021-07-16 16:14:41 +0100

    added error handling

--- lambda_function.py
+++ lambda_function.py
@@ -8,21 +8,29 @@ MAX_LTV = os.environ.get('MAX_LTV')
 def lambda_handler(event, context):
     """ Instantiate calculator and return values in a response object """
 
-    loan_amount = event['loan_amount']
-    property_value = event['property_value']
-    max_ltv = MAX_LTV
+    try:
+        loan_amount = float(event['loan_amount'])
+        property_value = float(event['property_value'])
+        
+        max_ltv = float(MAX_LTV)
 
-    ltv_calculator = LTVCalculator(loan_amount, property_value, max_ltv)
+        ltv_calculator = LTVCalculator(loan_amount, property_value, max_ltv)
 
-    ltv_percentage = ltv_calculator.get_value()
-    is_acceptable = ltv_calculator.is_acceptable()
+        ltv_percentage = ltv_calculator.get_value()
+        is_acceptable = ltv_calculator.is_acceptable()
 
-    data = {
-        "ltv_percentage": ltv_percentage,
-        "is_acceptable": is_acceptable
-    }
+        data = {
+            "ltv_percentage": ltv_percentage,
+            "is_acceptable": is_acceptable
+        }
 
-    return {
-        "statusCode": 200,
-        "body": data
-    }
+        return {
+            "statusCode": 200,
+            "body": data
+        }
+    except Exception as e:
+        return {
+            "statusCode": 400,
+            "body": str(e)
+        }
+
